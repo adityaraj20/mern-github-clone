7396ddddb5a3ff183ff41f02c7e1ad1203c7dda1
angrypotatoe19 <adityaroxx333@gmail.com>
2024-06-22 16:58:37 +0530

    api routes setup

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -18,23 +18,15 @@ const HomePage = () => {
     async (username = "adityaraj20") => {
       setLoading(true);
       try {
-        const userRes = await fetch(
-          `https://api.github.com/users/${username}`,
-          {
-            headers: {
-              authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-            },
-          }
+        const res = await fetch(
+          `http://localhost:5000/api/users/profile/${username}`
         );
-
-        const userProfile = await userRes.json();
-        setUserProfile(userProfile);
-
-        const repoRes = await fetch(userProfile.repos_url);
-        const repos = await repoRes.json();
+        const { repos, userProfile } = await res.json();
 
         repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
+
         setRepos(repos);
+        setUserProfile(userProfile);
 
         return { userProfile, repos };
       } catch (error) {
